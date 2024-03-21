ac75481233ee63414c0d949362ccac37a6011724
지수빈 <jyepjhhhh@gmail.com>
2024-03-21 14:33:00 +0900

    modified:   src/main/java/com/smhrd/botbuddies/controller/StoreController.java 	modified:   src/main/java/com/smhrd/botbuddies/controller/UserController.java 	modified:   src/main/resources/mapper/StoreMapper.xml

--- src/main/java/com/smhrd/botbuddies/controller/UserController.java
+++ src/main/java/com/smhrd/botbuddies/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.smhrd.botbuddies.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +10,6 @@ import com.smhrd.botbuddies.mapper.UserMapper;
 
 import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -20,26 +17,6 @@ public class UserController {
 
     @Autowired
     private UserMapper mapper;
-
-    @RequestMapping("/search")
-    public ResponseEntity<String> handleSearch(@RequestBody Map<String, String> requestData) {
-        if ("성공".equals(requestData.get("data"))) {
-        	System.out.println(requestData.get("data"));
-            // 요청 데이터가 '성공'이면 '완료'를 응답합니다.
-            return ResponseEntity.ok("완료");
-        }
-        return ResponseEntity.badRequest().body("실패");
-    }
-
-    @RequestMapping("/search_2")
-    public ResponseEntity<String> handleSearch2(@RequestBody Map<String, String> requestData) {
-        if ("성공".equals(requestData.get("data"))) {
-        	System.out.println(requestData.get("data"));
-            // 요청 데이터가 '성공'이면 '완료'를 응답합니다.
-            return ResponseEntity.ok("완료");
-        }
-        return ResponseEntity.badRequest().body("실패");
-    }
 
     @RequestMapping("/user_info")
     public List<User> user_info(@RequestBody Map<String, String> requestData) {
